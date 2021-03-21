d2357a365cec7f5236deab4acd92f83f308cbb39
Oscar Nilsson <oscar.nilsson@qte.se>
2021-03-21 23:42:27 +0100

    added a sunday evening log visualizer

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const { getSize, getInstructions } = require("./helpers/argvParse.js");
-const Console = require("./classes/Console.js");
+const { getSize, getInstructions, getStartPos } = require("./helpers/argvParse.js");
+const visualizeGrid = require("./helpers/visualizeGrid.js")
+
 const Object = require("./classes/Object.js");
 const Table = require("./classes/Table.js");
 
 const size = getSize(process.argv);
 const instructions = getInstructions(process.argv);
+const startingPos = getStartPos(process.argv);
 
 const t = new Table(size);
-const o = new Object({});
+const o = new Object({ position: startingPos });
 
 // I would rather initiate this middleware style,
 // using an array of fns and reduceRight
 // However, weekend brain can't think properly
 o.onMove = (move) => {
+  /**
+   * Created a way to visually
+   * make sure everything worked
+   * properly. Blame that it's
+   * sunday evening already
+   */
+
+  // visualizeGrid(t, move)
   try {
     if (!t.checkMove(move)) t.fellOutside();
     // if it didnt fall outside
