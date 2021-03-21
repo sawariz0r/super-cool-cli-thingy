5f20ce0d8cc24c7458c27c8797ee520084ef1223
Oscar Nilsson <oscar.nilsson@qte.se>
2021-03-22 00:14:48 +0100

    updated comment

---
+++ utils/visualizeGrid.js
@@ -0,0 +1,16 @@
+module.exports = (t, move) => {
+  /**
+   * As just using the spread operator
+   * won't make a deep copy of the
+   * grid, I had to use this "hacky"
+   * way to make a deep copy without
+   * the reference to the original
+   * array. 
+   * 
+   * In a future version this could potentially
+   * show the path with arrows. But not today.
+   */
+  const vGrid = JSON.parse(JSON.stringify(t.grid));
+  vGrid[move.y][move.x] = "X";
+  console.log(vGrid)
+}
