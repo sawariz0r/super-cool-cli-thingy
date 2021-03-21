405fdf06e2794ac9b29576610ad2cc9e9fb65600
Oscar Nilsson <oscar.nilsson@qte.se>
2021-03-21 23:13:29 +0100

    Added table comments and formatting

--- classes/Table.js
+++ classes/Table.js
@@ -6,18 +6,42 @@ class Table {
   }
 
   grid(height, width) {
+    /**
+     * Creating a two dimensional grid array,
+     * however it's not neccesary to add the
+     * coordinates to the objects. But the
+     * method I went with to check if the
+     * space exists in checkMove requires me
+     * to have something in there.
+     * Also could be useful if you decide to
+     * add functionality to rotate the
+     * table.
+     */
     return Array.from(new Array(height), (_, x) =>
       Array.from(new Array(width), (_, y) => {
-        return [y, x]
+        return [y, x];
       })
     );
   }
 
   checkMove({ x, y }) {
-    return !!this.grid[y][x]
+    /**
+     * Checks if the space exists,
+     * and turns it into a boolean.
+     * Returns if the move is valid
+     * or not.
+     */
+    return !!this.grid[y][x];
   }
 
   fellOutside() {
+    /**
+     * It made sense to
+     * put this here for now,
+     * instead of using it in
+     * multiple places where
+     * the table was initialized
+     */
     console.log([-1, -1]);
     process.exit();
   }
